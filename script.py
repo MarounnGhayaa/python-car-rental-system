c5022a2396a3068a25fa5c36226b355e5a452762
MarounnGhayaa <marounghaya@icloud.com>
2025-02-26 01:54:01 +0200

    Modifying names and functions to clarify the output

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-class Car:
+class Vehicle:
  def __init__(self, brand, model, year, rental_price_per_day):
     self.brand = brand
     self.model = model
@@ -6,14 +6,15 @@ class Car:
     self.rental_price_per_day = rental_price_per_day
 
  def display_info(self):
-   print(self.brand, self.model, self.year, self.rental_price_per_day)
+   print(f"{self.brand}: {self.model}, Year: {self.year}, Rental Price: ${self.rental_price_per_day}/day")
 
  def calculate_rental_cost(self, days):
-   cost = self.rental_price_per_day * 24 * days
-   print(cost)
+   cost = self.rental_price_per_day * days
+   print(f"Rental cost for {self.brand} {self.model} for {days} days: ${cost}")
 
-car1 = Car("mercedes", "e320", 2003, 25)
-car2 = Car("bmw", "x5", 2013, 50)
-car1.display_info()
-car2.display_info()
-car2.calculate_rental_cost(5)
+vehicle1 = Vehicle("Car", "Toyota Corolla", 2020, 50)
+vehicle2 = Vehicle("Bike", "Yamaha R1", 2019, 30)
+vehicle1.display_info()
+vehicle2.display_info()
+vehicle1.calculate_rental_cost(3)
+vehicle2.calculate_rental_cost(5)
