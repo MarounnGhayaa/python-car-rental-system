85ce279eab4e14433f14629fbd11a77566a2dca4
MarounnGhayaa <marounghaya@icloud.com>
2025-02-26 01:33:04 +0200

    Creating a function to calculate rent per day

--- script.py
+++ script.py
@@ -1,14 +1,19 @@
 class Car:
-  def __init__(self, brand, model, year, rental_price_per_day):
+ def __init__(self, brand, model, year, rental_price_per_day):
     self.brand = brand
     self.model = model
     self.year = year
     self.rental_price_per_day = rental_price_per_day
 
-  def display_info(self):
-    print(self.brand, self.model, self.year, self.rental_price_per_day)
+ def display_info(self):
+   print(self.brand, self.model, self.year, self.rental_price_per_day)
+
+ def calculate_rental_cost(self, days):
+   cost = self.rental_price_per_day * 24 * days
+   print(cost)
 
 car1 = Car("mercedes", "e320", 2003, 25)
 car2 = Car("bmw", "x5", 2013, 50)
 car1.display_info()
 car2.display_info()
+car2.calculate_rental_cost(5)
